9ec08d0a275fdee1307f1c539faacf0c8ee17aef
Yu Okamoto <yuokamoto1988@gmail.com>
2023-12-11 10:14:09 +0900

    pubsub example

--- setup.py
+++ setup.py
@@ -20,6 +20,8 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+                'talker = py_pubsub.publisher_member_function:main',
+                'listener = py_pubsub.subscriber_member_function:main',
         ],
     },
 )
