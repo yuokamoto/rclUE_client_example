db24db226f8308781ff500e284c5d8af56951af1
Yu Okamoto <yuokamoto1988@gmail.com>
2024-05-17 23:49:02 +0900

    wip

--- rclUE_client_example/rclUE_client_example/warehouse_client.py
+++ rclUE_client_example/rclUE_client_example/warehouse_client.py
@@ -13,10 +13,12 @@
 # limitations under the License.
 import time
 import random
+import quaternion
+
 import rclpy
 from rclpy.node import Node
-from example_interfaces.msg import Int32, Float32, Int32MultiArray, Bool
-from geometry_msgs.msg import PoseStamped, Quaternion
+from example_interfaces.msg import Int32, Float32, Int32MultiArray, Bool, String
+from geometry_msgs.msg import PoseStamped, Quaternion, PointStamped, Pose, Point
 
 from .common import ExternalDeviceClient, ModelNames, AIMoveMode, array_to_vector_param, array_to_pose_param
 
@@ -67,13 +69,15 @@ class WarehouseClient(Node):
                 'goal_actor': 'BP_Cart',
                 # 'approach_location':[]
                 # 'approach_actor':
-                'use_default_apporach_location': True
+                'use_default_apporach_location': True,
+                'completed': False
             },
             {
                 'task_id': 'task_2',
                 'task_type': 'Drop', 
                 'goal_actor': 'DropPoint',
                 'approach_actor': 'ApproachPoint2'
+                'completed': False
             }
             
         ]
@@ -82,6 +86,12 @@ class WarehouseClient(Node):
         # create task list for agents
         # create pub/sub
     
+    def check_task_dependency(self):
+        # get list o task
+        # get list of tasks with dependency
+        # return true if dependent task is completed
+        pass
+
     def process_order(self):
         print('process_order')
         for agent in self.agent_list:
@@ -103,33 +113,81 @@ class WarehouseClient(Node):
                 approach_location = task['approach_location'] if 'approach_location' in task else None
                 approach_actor = task['approach_actor'] if 'approach_actor' in task else None
             
-                if task['task_type'] == 'Move':
-                    pass
-                else:
-                    # param parser
+                # parse goal location
+                goal_actor_msg = goal_location_msg = goal_orientation_msg = point_stamped_msg = pose_stamped_msg = None
+                if goal_actor is not None and goal_actor != '':
+                    goal_actor_msg = String()
+                    goal_actor_msg.data = goal_actor
+
+                elif goal_loction is not None and len(goal_loction) >= 3:
+                    goal_location_msg = Point()
+                    goal_location_msg.x = goal_location[0]
+                    goal_location_msg.y = goal_location[1]
+                    goal_location_msg.z = goal_location[2]
+
+                    point_stamped_msg = PointStamped()
+                    point_stamped_msg.header.frame_id = self.reference_frame
+                    point_stamped_msg.point = goal_location_msg
+                
+                elif goal_orientation is not None and len(goal_orientation) >= 3:
+                    goal_orientation_msg = Orientation()
+                    q = quaternion.from_euler_angles(goal_orientation[0], goal_orientation[1], goal_orientation[2])
+                    goal_orientation_msg.x = q.x
+                    goal_orientation_msg.y = q.y
+                    goal_orientation_msg.z = q.z
+                    goal_orientation_msg.w = q.w
+                
+                if goal_location_msg is not None and goal_orientation_msg is not None:
+                    pose_stamped_msg = PoseStamped()
+                    pose_stamped_msg.header.frame_id = self.reference_frame
+                    pose_stamped_msg.pose.position = goal_location_msg
+                    pose_stamped_msg.pose.orientation = goal_orientation_msg
+
+                # process task
+                if task['task_type'] == 'Move': # nav
+                    if goal_actor_msg is not None:
+                        agent.actor_goal_publisher_.publish(goal_actor_msg)
+                    elif pose_stamped_msg is not None:
+                        agent.pose_goal_publisher_.publish(pose_stamped_msg)  
+                    else:
+                        print('Move task requires target actor or pose.')
+                        continue
+                else: # pick/drop
+                    # approach location
                     if use_default_apporach_location:
-                        msg = Bool()
-                        msg.data = True
-                        agent.set_use_default_approach_publisher_.publish(msg)
+                        bool_msg = Bool()
+                        bool_msg.data = True
+                        agent.set_use_default_approach_publisher_.publish(bool_msg)
                         
-                    elif approach_location is not None:
-                        pass
                     elif approach_actor is not None and approach_actor != '':
-                        pass
+                        approach_actor_msg = String()
+                        approach_actor_msg.data = approach_actor
+                        agent.set_approach_location_actor_publisher_.publish(approach_actor_msg)
+
+                    elif approach_location is not None and len(approach_location) >= 3:
+                        approach_location_msg = PointStamped()
+                        approach_location_msg.header.frame_id = self.reference_frame
+                        approach_location_msg.point.x = approach_location[0]
+                        approach_location_msg.point.y = approach_location[1]
+                        approach_location_msg.point.z = approach_location[2]
+                        agent.set_approach_location_publisher_.publish(approach_location_msg)
                     
                     if task['task_type'] == 'Pick':
-                        pass
+                        if goal_actor_msg is not None:
+                            agent.pick_actor_goal_publisher_.publish(goal_actor_msg)
+                        elif point_stamped_msg is not None:
+                            agent.pick_goal_publisher_.publish(point_stamped_msg)  
+                        else:
+                            print('Pick task requires target actor or position.')
+                            continue
                     elif task['task_type'] == 'Drop':
-                        pass
-                
-
-
-            # check task is none and navigation is none
-            # if task is none then assign task
-
-
-        # time.sleep(1)
-        
+                        if goal_actor_msg is not None:
+                            agent.drop_actor_goal_publisher_.publish(goal_actor_msg)
+                        elif pose_stamped_msg is not None:
+                            agent.drop_goal_publisher_.publish(pose_stamped_msg)  
+                        else:
+                            print('Drop task requires target actor or pose.')
+                            continue        
 
 def main(args=None):
     rclpy.init(args=args)
